b70cd6b78adca542c52e1b3f5d994f5138e51b2c
betsy18lyna-oss <betsy18lyna@gmail.com>
2025-08-26 10:28:20 -0500

    version 1
    desplegar vistas html

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+
 
 # Create your views here.
+
+
+def signup(request) :
+    return render(request, "signup.html",
+                  {"form":UserCreationForm})
