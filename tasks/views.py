301b360c83f549b9f96a32dadefe2b241054b071
betsy18lyna-oss <betsy18lyna@gmail.com>
2025-09-04 10:40:57 -0500

    djangocrud
    desplegar

--- tasks/views.py
+++ tasks/views.py
@@ -1,21 +1,44 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
-from django.contrib.auth import login
+from django.contrib.auth import login, logout, authenticate
 from django.db import IntegrityError
+
+
 
 # Create your views here.
 def home(request):
     return render(request, "home.html")
 
+def signout(request):
+    logout(request)
+    return redirect("home")
+
 def tasks(request):
     return render(request, "tasks.html")
 
+def signin(request):
+    if request.method == "GET":
+         return render(request,
+                        "signin.html",
+                        {"form":AuthenticationForm()})
+    else:
+        user = authenticate (request,
+                          username=request.POST["username"],
+                          password=request.POST["password"])
+        if user is None:
+            return render(request,
+                          "signin.html",
+                          {"form":AuthenticationForm(),
+                           "error":"Usuario o contraseña incorrecta"})
+        else:
+            login(request,user)
+            return redirect("tasks")
+
 def signup(request):
     if request.method == "GET":
-        return render(request, 
-                  "signup.html",
+        return render(request, "signup.html",
                   {"form":UserCreationForm})
     else:
         if request.POST["password1"] == request.POST["password2"]:
